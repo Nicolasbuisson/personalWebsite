a4ccfe0a8e7676d3d9a73993bf9aa1efe058045b
Nicolasbuisson <nicolas.buisson@mail.mcgill.ca>
2026-04-28 11:36:09 -0400

    header button appear on scroll once passed homeHero

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,42 @@
 "use client";
-import { useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import styles from "./header.module.css";
 import { AnimatePresence } from "framer-motion";
 import { Nav } from "../nav/nav";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const burger = useRef<HTMLDivElement>(null);
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(burger.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: 0,
+        end: window.innerHeight,
+        onLeave: () => {
+          gsap.to(burger.current, {
+            scale: 1,
+            duration: 0.25,
+            ease: "power1.out",
+          });
+        },
+        onEnterBack: () => {
+          gsap.to(burger.current, {
+            scale: 0,
+            duration: 0.25,
+            ease: "power1.out",
+          });
+        },
+      },
+    });
+  }, []);
 
   return (
     <>
-      <div className={styles.header}>
+      <div ref={burger} className={styles.header}>
         <div
           onClick={() => {
             setIsOpen(!isOpen);
