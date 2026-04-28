9cb225b8f9641c90d1a9f6b1b39f217fe6c2ca77
Nicolasbuisson <nicolas.buisson@mail.mcgill.ca>
2026-04-28 14:43:05 -0400

    allow FlipLink to have a span instead of an <a>

--- src/components/flipLink/flipLink.tsx
+++ src/components/flipLink/flipLink.tsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion";
 import styles from "./flipLink.module.css";
 
 interface FlipLinkProps {
-  href: string;
   label: string;
+  href?: string;
   onClick?: () => void;
 }
 
@@ -12,7 +12,7 @@ export const FlipLink = (props: FlipLinkProps) => {
   const DURATION = 0.25;
   const STAGGER = 0.025;
   const { href, label, onClick = () => {} } = props;
-  return (
+  return href ? (
     <motion.a
       initial="initial"
       whileHover="hovered"
@@ -65,5 +65,57 @@ export const FlipLink = (props: FlipLinkProps) => {
         ))}
       </div>
     </motion.a>
+  ) : (
+    <motion.span
+      initial="initial"
+      whileHover="hovered"
+      className={styles.flipLink}
+      onClick={onClick}
+    >
+      <div>
+        {label.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: 0,
+              },
+              hovered: {
+                y: "-100%",
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+      <div>
+        {label.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: "100%",
+              },
+              hovered: {
+                y: 0,
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+    </motion.span>
   );
 };
