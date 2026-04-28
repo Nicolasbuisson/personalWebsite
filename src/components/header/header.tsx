06ae1baaf12b25f92ee1bc6e08f41413c1f14698
Nicolasbuisson <nicolas.buisson@mail.mcgill.ca>
2026-04-28 16:08:55 -0400

    header responsiveness for mobile + prevent scrolling when header menu isOpen

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,13 +5,35 @@ import { AnimatePresence } from "framer-motion";
 import { Nav } from "../nav/nav";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Copyright } from "../copyright/copyright";
+import { FlipLink } from "../flipLink/flipLink";
+import { useLenis } from "lenis/react";
+
+const navItems = [
+  {
+    title: "Work",
+    href: "/work",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+];
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const burger = useRef<HTMLDivElement>(null);
+
+  const lenis = useLenis(); // to enable/disable scrolling
+
   useLayoutEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
-    gsap.to(burger.current, {
+    const tween = gsap.to(burger.current, {
       scrollTrigger: {
         trigger: document.documentElement,
         start: 0,
@@ -32,6 +54,10 @@ export const Header = () => {
         },
       },
     });
+    () => {
+      tween.kill();
+      tween.scrollTrigger?.kill();
+    };
   }, []);
 
   return (
@@ -39,6 +65,14 @@ export const Header = () => {
       <div ref={burger} className={styles.header}>
         <div
           onClick={() => {
+            gsap.to(burger.current, {
+              scale: isOpen
+                ? 0 // closing
+                : 1, // opening
+              duration: 0.25,
+              ease: "power1.out",
+            });
+            isOpen ? lenis?.start() : lenis?.stop();
             setIsOpen(!isOpen);
           }}
           className={styles.button}
@@ -48,12 +82,41 @@ export const Header = () => {
           ></div>
         </div>
       </div>
-
       <AnimatePresence mode="wait">
         {isOpen && (
           <Nav framerMotionExitAnimKey="key-to-animate-exit-transition-framer" />
         )}
       </AnimatePresence>
+      <nav className={styles.nav}>
+        <Copyright />
+        <li className={styles.menuLabel}>
+          <FlipLink
+            label={"Menu"}
+            onClick={() => {
+              // opening
+              gsap.to(burger.current, {
+                scale: 1,
+                delay: 0.5,
+                duration: 0.25,
+                ease: "power1.out",
+              });
+              lenis?.stop();
+              setIsOpen(!isOpen);
+            }}
+          />
+          <div className={styles.indicator} />
+        </li>
+        <ul>
+          {navItems.map((item) => {
+            return (
+              <li key={item.title} className={styles.headerNavLink}>
+                <FlipLink href={item.href} label={item.title} />
+                <div className={styles.indicator} />
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </>
   );
 };
--- src/components/homeHero/homeHero.tsx
+++ src/components/homeHero/homeHero.tsx
@@ -1,41 +1,12 @@
-import { Copyright } from "../copyright/copyright";
-import { FlipLink } from "../flipLink/flipLink";
 import { Header } from "../header/header";
 import { SlidingText } from "../slidingText/slidingText";
 import styles from "./homeHero.module.css";
-
-const navItems = [
-  {
-    title: "Work",
-    href: "/work",
-  },
-  {
-    title: "About",
-    href: "/about",
-  },
-  {
-    title: "Contact",
-    href: "/contact",
-  },
-];
 
 export const HomeHero = () => {
   return (
     <main className={styles.main}>
       <Header />
-      <nav className={styles.nav}>
-        <Copyright />
-        <ul>
-          {navItems.map((item) => {
-            return (
-              <li key={item.title}>
-                <FlipLink href={item.href} label={item.title} />
-                <div className={styles.indicator} />
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+
       <SlidingText />
     </main>
   );
